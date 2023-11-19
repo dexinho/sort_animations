8bd3bde5d8347aae4fcbd6c101893c259b3e7c1f
Jasmin Hidanovic <dexinho008@gmail.com>
2023-11-19 19:30:18 +0100

    elements selected via query selectors

---
+++ src/utility/querySelectors.js
@@ -0,0 +1,5 @@
+const sortAlgorithmDiv = document.querySelector("#sort-algorithm-div");
+const numbersDiv = document.querySelector("#numbers-div");
+const algoBtns = document.querySelectorAll(".algo-btns");
+
+export { sortAlgorithmDiv, numbersDiv, algoBtns };
