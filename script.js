b16afd0cb64d86dab7076b4f29190ef31dcde530
Jasmin Hidanovic <dexinho008@gmail.com>
2023-11-19 19:26:27 +0100

    created initial script

---
+++ script.js
@@ -0,0 +1,5 @@
+import appendNumbers from "./src/utility/appendNumbers.js";
+import attachAlgoBtnListeners from "./src/utility/attachAlgoBtnListeners.js";
+
+appendNumbers();
+attachAlgoBtnListeners()
