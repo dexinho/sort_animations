acf634075534d8f4d524f57200d7cf38c6556456
Jasmin Hidanovic <dexinho008@gmail.com>
2023-11-21 23:35:18 +0100

    now it only clears timeouts

--- src/utility/playbackBtns.js
+++ src/utility/playbackBtns.js
@@ -1,19 +1,17 @@
 import { pauseBtn, reverseBtn } from "./querySelectors.js";
 import { ALGO_DATA, ANIMATION_DATA } from "./globalVar.js";
-import startAnimation from "./startAnimation.js";
+import animationSetup from "./animationSetup.js";
 import { clearIntervals, clearTimeouts } from "./clearAsync.js";
-import { numberBlocks } from "./getElements.js";
 
 const playbackBtns = () => {
   pauseBtn.addEventListener("click", () => {
     if (pauseBtn.innerText === "PAUSE") {
-      clearIntervals(ANIMATION_DATA.animationIntervals);
       clearTimeouts(ANIMATION_DATA.animationTimeouts);
       pauseBtn.innerText = "RESUME";
     } else if (pauseBtn.innerText === "RESUME") {
       pauseBtn.innerText = "PAUSE";
 
-      startAnimation({
+      animationSetup({
         pairMoves: ALGO_DATA.pairMoves,
         currentMove: ALGO_DATA.currentMove,
         direction: true,
@@ -22,18 +20,10 @@ const playbackBtns = () => {
   });
 
   reverseBtn.addEventListener("click", () => {
-    // [...numberBlocks].forEach((block) =>
-    //   block.classList.remove("highlight-blocks")
-    // );
-    clearIntervals(ANIMATION_DATA.animationIntervals);
-    clearTimeouts(ANIMATION_DATA.animationTimeouts);
-    setTimeout(() => {
-      startAnimation({
-        pairMoves: ALGO_DATA.pairMoves,
-        currentMove: ALGO_DATA.currentMove,
-        direction: false,
-      });
-    }, 1500);
+    animationSetup({
+      pairMoves: ALGO_DATA.pairMoves,
+      direction: false,
+    });
   });
 };
 
