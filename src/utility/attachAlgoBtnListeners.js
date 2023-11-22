064f8ae6420b5804de7a612999e6e6e11da4c616
Jasmin Hidanovic <dexinho008@gmail.com>
2023-11-22 04:06:19 +0100

    calculated total animation delay

--- src/utility/attachAlgoBtnListeners.js
+++ src/utility/attachAlgoBtnListeners.js
@@ -1,20 +1,26 @@
 import { algoBtns } from "./querySelectors.js";
 import bubbleSort from "./bubbleSort.js";
-import animationSetup from "./animationSetup.js";
+import { animationSetup } from "./animationSetup.js";
 import insertSort from "./insertSort.js";
+import { clearTimeouts } from "./clearAsync.js";
+import { ANIMATION_DATA } from "./globalVar.js";
+import { totalAnimationDelay } from "./animationSetup.js";
 
 const attachAlgoBtnListeners = () => {
   algoBtns.forEach((btn) => {
     btn.addEventListener("click", () => {
       let pairMoves;
+      ANIMATION_DATA.pause = false;
       if (btn.id === "bubble-sort-btn") {
         pairMoves = bubbleSort();
       } else if (btn.id === "insert-sort-btn") {
         pairMoves = insertSort();
       }
 
-      algoBtns.forEach(button => button.style.display = 'none')
-      animationSetup({ pairMoves, direction: true });
+      algoBtns.forEach((button) => (button.style.display = "none"));
+      totalAnimationDelay()
+      clearTimeouts(ANIMATION_DATA.animationTimeouts);
+      animationSetup({ pairMoves, isDirectionForward: true });
     });
   });
 };
