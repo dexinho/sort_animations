84b98d6761f42609c5f3f49c77a4176e6a4db58c
Jasmin Hidanovic <dexinho008@gmail.com>
2023-11-22 04:07:04 +0100

    added aditional prop for animation_data

--- src/utility/globalVar.js
+++ src/utility/globalVar.js
@@ -1,13 +1,17 @@
 const ALGO_DATA = {
-  initialArr: [7, 3, 4, 6, 5, 8, 9, 2, 1],
+  initialArr: [5, 6, 1, 4, 2, 9, 8, 3, 7],
   currentMove: 0,
   pairMoves: [],
 };
 
 const ANIMATION_DATA = {
+  isAnimationInProgress: true,
+  isAnimationPaused: false,
   animationTimeouts: [],
   animationIntervals: [],
   animationSpeedMS: 2000,
+  delay: 500,
+  totalDelay: 0,
 };
 
 export { ALGO_DATA, ANIMATION_DATA };
