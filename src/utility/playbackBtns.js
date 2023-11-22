aebfb7b7efaf7ea903db457a39d8bb35d006f87f
Jasmin Hidanovic <dexinho008@gmail.com>
2023-11-22 04:06:45 +0100

    added try/catch that handles if animation is in progress

--- src/utility/playbackBtns.js
+++ src/utility/playbackBtns.js
@@ -1,29 +1,51 @@
 import { pauseBtn, reverseBtn } from "./querySelectors.js";
 import { ALGO_DATA, ANIMATION_DATA } from "./globalVar.js";
-import animationSetup from "./animationSetup.js";
-import { clearIntervals, clearTimeouts } from "./clearAsync.js";
+import { animationSetup } from "./animationSetup.js";
+import { clearTimeouts } from "./clearAsync.js";
 
 const playbackBtns = () => {
   pauseBtn.addEventListener("click", () => {
     if (pauseBtn.innerText === "PAUSE") {
-      clearTimeouts(ANIMATION_DATA.animationTimeouts);
       pauseBtn.innerText = "RESUME";
+      ANIMATION_DATA.isAnimationPaused = true;
     } else if (pauseBtn.innerText === "RESUME") {
       pauseBtn.innerText = "PAUSE";
-
+      ANIMATION_DATA.isAnimationPaused = false;
       animationSetup({
         pairMoves: ALGO_DATA.pairMoves,
-        currentMove: ALGO_DATA.currentMove,
-        direction: true,
+        isDirectionForward: true,
       });
     }
   });
 
   reverseBtn.addEventListener("click", () => {
-    animationSetup({
-      pairMoves: ALGO_DATA.pairMoves,
-      direction: false,
-    });
+    let isDirectionForward = false;
+    ANIMATION_DATA.isAnimationPaused = false;
+    if (reverseBtn.innerText === "REVERSE") {
+      pauseBtn.innerText = "PAUSE";
+      reverseBtn.innerText = "FORWARD";
+    } else if (reverseBtn.innerText === "FORWARD") {
+      isDirectionForward = true;
+      reverseBtn.innerText = "REVERSE";
+    }
+
+    ANIMATION_DATA.isAnimationPaused = true;
+
+    const reverseDirection = () => {
+      if (ANIMATION_DATA.isAnimationInProgress) {
+        clearInterval(directionInterval);
+        clearTimeouts(ANIMATION_DATA.animationTimeouts);
+        ANIMATION_DATA.isAnimationPaused = false;
+        ALGO_DATA.currentMove = isDirectionForward
+          ? ++ALGO_DATA.currentMove
+          : --ALGO_DATA.currentMove;
+        animationSetup({
+          pairMoves: ALGO_DATA.pairMoves,
+          isDirectionForward,
+        });
+      }
+    };
+    const directionInterval = setInterval(reverseDirection, 1);
   });
 };
 
